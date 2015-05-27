c78246dc998eb3f689215bb90cc15381ee496b18
hu_bh <hub830@126.com>
2015-05-27 08:34:24 +0800

    add controller

--- test8/public/javascript/app.js
+++ test8/public/javascript/app.js
@@ -1,1 +1,20 @@
-aa
+'use strict';
+
+/* App Module */
+
+var test8App = angular.module('test8App', [
+  'ngRoute',
+  'test8Controllers'
+]);
+
+test8App.config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider.
+      when('/', {
+        templateUrl: 'partials/index.html',
+        controller: 'indexCtrl'
+      }).
+      otherwise({
+        redirectTo: '/'
+      });
+  }]);
